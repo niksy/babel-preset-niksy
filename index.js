1bb61cdc2d942003f89159454faca22775df6bf0
Ivan Nikolić <ivan.nikolic@trikoder.net>
2017-05-22 11:48:02 +0200

    Change configuration definition

--- index.js
+++ index.js
@@ -4,19 +4,18 @@
 
 module.exports = {
 	plugins: [
-		require('babel-plugin-transform-es2015-function-name'),
-		require('babel-plugin-transform-es2015-arrow-functions'),
-		[require('babel-plugin-transform-es2015-template-literals'), {
+		'transform-es2015-function-name',
+		'transform-es2015-arrow-functions',
+		['transform-es2015-template-literals', {
 			spec: true
 		}],
-		require('babel-plugin-transform-es2015-block-scoping'),
-		require('babel-plugin-check-es2015-constants'),
-		require('babel-plugin-transform-es2015-shorthand-properties'),
-		[require('babel-plugin-transform-es2015-computed-properties'), {
+		'transform-es2015-block-scoping',
+		'check-es2015-constants',
+		'transform-es2015-shorthand-properties',
+		['transform-es2015-computed-properties', {
 			loose: true
 		}],
-
-		require('babel-plugin-transform-es3-member-expression-literals'),
-		require('babel-plugin-transform-es3-property-literals')
+		'transform-es3-member-expression-literals',
+		'transform-es3-property-literals'
 	]
 };
