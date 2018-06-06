6bd5bf5dd501c5ea89ae872d8aea8a1f2c188112
Ivan Nikolić <niksy5@gmail.com>
2018-06-06 17:58:45 +0200

    Improve Node support

--- test/index.js
+++ test/index.js
@@ -9,50 +9,68 @@ function getCodeFromFile ( file ) {
 	return fs.readFileSync(path.join(__dirname, file), 'utf8');
 }
 
-function runBabel ( file, config ) {
-	return `${babel.transform(getCodeFromFile(file), config).code}\n`;
+function runBabel ( file, config, cb ) {
+	return babel.transformFile(path.join(__dirname, file), config, function ( err, result ) {
+		if ( err ) {
+			throw err;
+		}
+		cb(`${result.code}\n`);
+	});
 }
 
 describe('Basic', function () {
 
-	it('should properly transform code', function () {
-		const transformedCode = runBabel('./fixtures/basic.input.js', {
+	if ( /^4\./.test(process.versions.node) ) {
+		// Node 4 has sometimes racing condition problems with babel-preset-env
+		this.timeout(5000);
+	}
+
+	it('should properly transform code', function ( done ) {
+		runBabel('./fixtures/basic.input.js', {
 			presets: require.resolve('../')
+		}, function ( transformedCode ) {
+			assert.equal(transformedCode, getCodeFromFile('./fixtures/basic.output.js'));
+			done();
 		});
-		assert.equal(transformedCode, getCodeFromFile('./fixtures/basic.output.js'));
 	});
 
 });
 
 describe('Next', function () {
 
-	it('should properly transform code', function () {
-		const transformedCode = runBabel('./fixtures/next.input.js', {
+	it('should properly transform code', function ( done ) {
+		runBabel('./fixtures/next.input.js', {
 			presets: require.resolve('../next')
+		}, function ( transformedCode ) {
+			assert.equal(transformedCode, getCodeFromFile('./fixtures/next.output.js'));
+			done();
 		});
-		assert.equal(transformedCode, getCodeFromFile('./fixtures/next.output.js'));
 	});
 
 });
 
 describe('React', function () {
 
-	it('should properly transform code', function () {
-		const transformedCode = runBabel('./fixtures/react.input.js', {
+	it('should properly transform code', function ( done ) {
+		runBabel('./fixtures/react.input.js', {
 			presets: require.resolve('../react')
+		}, function ( transformedCode ) {
+			assert.equal(transformedCode, getCodeFromFile('./fixtures/react.output.js'));
+			done();
 		});
-		assert.equal(transformedCode, getCodeFromFile('./fixtures/react.output.js'));
 	});
 
 });
 
 describe('Vue', function () {
 
-	it('should properly transform code', function () {
-		const transformedCode = runBabel('./fixtures/vue.input.js', {
+	it('should properly transform code', function ( done ) {
+		runBabel('./fixtures/vue.input.js', {
 			presets: require.resolve('../vue')
+		}, function ( transformedCode ) {
+			assert.equal(transformedCode, getCodeFromFile('./fixtures/vue.output.js'));
+			done();
 		});
-		assert.equal(transformedCode, getCodeFromFile('./fixtures/vue.output.js'));
 	});
 
 });
